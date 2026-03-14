ce3891e7f9f6547d02f896fc7171067213d172f8
sudarshan <ssgurav196@gmail.com>
2026-03-14 19:07:05 +0530

    first website version

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("projectForm")
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault()
+
+    alert("Your project request has been submitted!")
+
+})
